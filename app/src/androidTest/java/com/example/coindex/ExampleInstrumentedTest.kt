9c7feff7139f23794029c3a34b49de8d720e4310
ByteSick <124615251+ByteSick@users.noreply.github.com>
2020-07-29 13:20:02 +0000

    Fix shadowed variables

---
+++ app/src/androidTest/java/com/example/coindex/ExampleInstrumentedTest.kt
@@ -0,0 +1,14 @@
+
+package com.example.coindex
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
