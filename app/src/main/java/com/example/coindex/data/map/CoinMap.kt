0efe6cfa37b4f056e97820aa680ce864512ae672
ByteSick <124615251+ByteSick@users.noreply.github.com>
2020-09-06 06:35:54 +0000

    more statistics

--- app/src/main/java/com/example/coindex/data/map/CoinMap.kt
+++ app/src/main/java/com/example/coindex/data/map/CoinMap.kt
@@ -3,4 +3,6 @@ package com.example.coindex.data.map
 import com.example.coindex.data.remote.dto.CoinDto
 import com.example.coindex.data.remote.dto.CoinInfoDto
 import com.example.coindex.domain.model.Coin
-import com.example.coindex.domain.mode
+import com.example.coindex.domain.model.CoinInfo
+
+    fun CoinDto.toCoin(): 
