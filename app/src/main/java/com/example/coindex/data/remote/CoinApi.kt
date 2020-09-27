67cfb40fb4c819ff19d79bdc3933c196723b6b9e
ByteSick <124615251+ByteSick@users.noreply.github.com>
2020-09-27 07:14:54 +0000

    Update "CHANGELOG" file.

--- app/src/main/java/com/example/coindex/data/remote/CoinApi.kt
+++ app/src/main/java/com/example/coindex/data/remote/CoinApi.kt
@@ -2,4 +2,6 @@ package com.example.coindex.data.remote
 
 import com.example.coindex.data.remote.dto.CoinDto
 import com.example.coindex.data.remote.dto.CoinInfoDto
-import retrofit2
+import retrofit2.http.GET
+import retrofit2.http.Path
+
