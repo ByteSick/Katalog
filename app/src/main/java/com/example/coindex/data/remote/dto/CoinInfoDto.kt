b1e5e95b0ef4393146b533f252d11268233e845a
ByteSick <124615251+ByteSick@users.noreply.github.com>
2020-10-31 19:05:56 +0000

    fix ?

--- app/src/main/java/com/example/coindex/data/remote/dto/CoinInfoDto.kt
+++ app/src/main/java/com/example/coindex/data/remote/dto/CoinInfoDto.kt
@@ -3,4 +3,5 @@ package com.example.coindex.data.remote.dto
 import com.example.coindex.domain.model.CoinInfo
 import com.google.gson.annotations.SerializedName
 
-data cl
+data class CoinInfoDto(
+    @SerializedName(
