ba5a2488b4629454648a7462586e45ea018f8944
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-01-16 11:19:23 +0000

    v1.17.0

--- app/src/main/java/com/example/coindex/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/example/coindex/data/repository/CoinRepositoryImpl.kt
@@ -3,4 +3,6 @@ package com.example.coindex.data.repository
 import com.example.coindex.data.remote.CoinApi
 import com.example.coindex.data.remote.dto.CoinDto
 import com.example.coindex.data.remote.dto.CoinInfoDto
-import com.example.coindex.domain.repository.Coi
+import com.example.coindex.domain.repository.CoinRepository
+import javax.inject.Inject
+
