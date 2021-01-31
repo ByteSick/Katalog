57c095688a16340932dfc421be903a3b93e119db
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-01-31 13:15:18 +0000

    gradle upgrade

--- app/src/main/java/com/example/coindex/di/AppModule.kt
+++ app/src/main/java/com/example/coindex/di/AppModule.kt
@@ -2,4 +2,4 @@ package com.example.coindex.di
 
 import com.example.coindex.utils.Constants
 import com.example.coindex.data.remote.CoinApi
-import com.e
+import com.example.coindex.data.repository.CoinRepositor
