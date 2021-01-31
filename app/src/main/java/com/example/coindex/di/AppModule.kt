4d52f82909ed9f2107b7f794d8006d98168c8b5d
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-01-31 20:14:44 +0000

    Syntax update

--- app/src/main/java/com/example/coindex/di/AppModule.kt
+++ app/src/main/java/com/example/coindex/di/AppModule.kt
@@ -2,4 +2,5 @@ package com.example.coindex.di
 
 import com.example.coindex.utils.Constants
 import com.example.coindex.data.remote.CoinApi
-import com.example.coindex.data.repository.CoinRepositor
+import com.example.coindex.data.repository.CoinRepositoryImpl
+import com.example.coi
