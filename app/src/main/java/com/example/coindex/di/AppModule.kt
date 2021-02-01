039de4b9819067936946cb9813ce19d5698b7499
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-02-01 17:01:00 +0000

    small extensions

--- app/src/main/java/com/example/coindex/di/AppModule.kt
+++ app/src/main/java/com/example/coindex/di/AppModule.kt
@@ -3,4 +3,5 @@ package com.example.coindex.di
 import com.example.coindex.utils.Constants
 import com.example.coindex.data.remote.CoinApi
 import com.example.coindex.data.repository.CoinRepositoryImpl
-import com.example.coi
+import com.example.coindex.domain.repository.CoinRepository
+i
