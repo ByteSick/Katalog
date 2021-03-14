63991b335a01dee68d2fa7168729a668f1016110
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-03-14 10:49:49 +0000

    Note changes

--- app/src/main/java/com/example/coindex/domain/use_case/get_coin/GetCoin.kt
+++ app/src/main/java/com/example/coindex/domain/use_case/get_coin/GetCoin.kt
@@ -1,4 +1,5 @@
 package com.example.coindex.domain.use_case.get_coin
 
 import com.example.coindex.utils.Resource
-import com.example.coindex.domain.mo
+import com.example.coindex.domain.model.CoinInfo
+import com.example.coind
