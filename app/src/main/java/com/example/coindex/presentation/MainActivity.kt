c00bfb070a0c7d6b605836c801a0d2fa26a22bb9
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-04-06 21:01:16 +0000

    dependencies updated

--- app/src/main/java/com/example/coindex/presentation/MainActivity.kt
+++ app/src/main/java/com/example/coindex/presentation/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.coindex.presentation
 
 import androidx.activity.ComponentActivity
 import android.os.Bundle
-import androidx.activity.compose.setConte
+import androidx.activity.compose.setContent
+import androidx.compose.material
