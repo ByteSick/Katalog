175360c079ccb042eea3c809b1a5a13eca526ed9
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-04-08 16:15:27 +0000

    Fix animations

--- app/src/main/java/com/example/coindex/presentation/MainActivity.kt
+++ app/src/main/java/com/example/coindex/presentation/MainActivity.kt
@@ -3,4 +3,5 @@ package com.example.coindex.presentation
 import androidx.activity.ComponentActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.material
+import androidx.compose.material.MaterialTheme
+import androidx.compo
