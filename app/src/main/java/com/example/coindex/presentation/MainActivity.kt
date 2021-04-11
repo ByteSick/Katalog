686dadb651d501cd569a960638682ead15edf3fe
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-04-11 21:40:47 +0000

    No change

--- app/src/main/java/com/example/coindex/presentation/MainActivity.kt
+++ app/src/main/java/com/example/coindex/presentation/MainActivity.kt
@@ -6,4 +6,5 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.navigation.compose.NavHost
-import androidx.na
+import androidx.navigation.compose.composable
+import androidx.
