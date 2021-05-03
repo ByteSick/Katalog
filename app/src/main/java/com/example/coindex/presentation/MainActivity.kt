d172c2299b7ade8b02d3129ba1a324697da072d5
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-05-03 11:56:27 +0000

    hound fixes

--- app/src/main/java/com/example/coindex/presentation/MainActivity.kt
+++ app/src/main/java/com/example/coindex/presentation/MainActivity.kt
@@ -29,4 +29,5 @@ class MainActivity : ComponentActivity() {
                     ) {
                         composable(
                             route = Screen.CoinListScreen.route
-                      
+                        ) {
+                        
