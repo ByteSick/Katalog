4f5e4194d5cf1b4da794c0914bb793ffa5823684
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-05-13 10:07:33 +0000

    No ramo master

--- app/src/main/java/com/example/coindex/presentation/Screen.kt
+++ app/src/main/java/com/example/coindex/presentation/Screen.kt
@@ -2,4 +2,5 @@ package com.example.coindex.presentation
 
 sealed class Screen(val route:String) {
     object CoinListScreen: Screen("coin_list_screen")
-    object CoinInfoScreen: Screen("coin_info_screen
+    object CoinInfoScreen: Screen("coin_info_screen")
+}
---
+++ app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoScreen.kt
@@ -0,0 +1,1 @@
+package com.example.coindex.p
