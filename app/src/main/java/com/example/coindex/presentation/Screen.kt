d0cfee5c88b88efd1ba33989cb019b6f371279fe
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-05-13 05:36:21 +0000

    Translated by douglas

--- app/src/main/java/com/example/coindex/presentation/Screen.kt
+++ app/src/main/java/com/example/coindex/presentation/Screen.kt
@@ -2,4 +2,4 @@ package com.example.coindex.presentation
 
 sealed class Screen(val route:String) {
     object CoinListScreen: Screen("coin_list_screen")
-    ob
+    object CoinInfoScreen: Screen("coin_info_screen
