4c011a2e6d56ebb157a6fb1fd7c43531927587e0
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-05-17 07:24:57 +0000

    exec: use -ELOOP for max recursion depth

--- app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoScreen.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoScreen.kt
@@ -3,4 +3,5 @@ package com.example.coindex.presentation.coin_info
 import android.content.res.Resources.Theme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.fo
+import androidx.compose.foundation.lazy.items
+import androidx.co
