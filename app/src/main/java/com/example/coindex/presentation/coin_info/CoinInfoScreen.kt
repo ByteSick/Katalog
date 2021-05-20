2a26c7f689dbc9f011450adfbfa69dce2f57f005
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-05-20 10:45:18 +0000

    fully working

--- app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoScreen.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoScreen.kt
@@ -5,4 +5,5 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Divid
+import androidx.compose.material.Divider
+import androidx.compose.material.M
