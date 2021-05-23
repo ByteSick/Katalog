a8ca2f8159d605bfb46155e4401079ece3065050
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-05-23 09:14:47 +0000

    - minor

--- app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoScreen.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoScreen.kt
@@ -11,4 +11,5 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.
+import androidx.compose.ui.graphics.Color
+import androidx.compose
