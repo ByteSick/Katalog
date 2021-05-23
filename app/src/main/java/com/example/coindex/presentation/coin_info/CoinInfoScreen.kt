17bc13bf4bd2c0af4ddad8a1feac23fb94e19b68
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-05-23 21:52:27 +0000

    Simplify logic

--- app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoScreen.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoScreen.kt
@@ -12,4 +12,5 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose
+import androidx.compose.ui.text.font.FontStyle
+import androidx
