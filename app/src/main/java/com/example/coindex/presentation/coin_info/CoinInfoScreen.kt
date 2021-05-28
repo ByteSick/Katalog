7e027833192944c6fca55718543e7fd933210573
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-05-28 17:15:52 +0000

    Messing around

--- app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoScreen.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoScreen.kt
@@ -16,4 +16,5 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.coindex.presentation.
+import com.example.coindex.presentation.coin_info.components.CoinTag
+import com.example
