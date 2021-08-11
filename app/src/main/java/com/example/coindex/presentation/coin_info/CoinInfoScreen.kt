6bb440f44a4f0a628578c8d88e663118f7e0097a
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-08-11 19:29:40 +0000

    home page updates

--- app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoScreen.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoScreen.kt
@@ -40,4 +40,4 @@ fun CoinInfoScreen(
                         Text(
                             text = "${coin.rank}. ${coin.name} (${coin.symbol})",
                             style = MaterialTheme.typography.h2,
-              
+                            modifier = Modifier.weight(8f
