945849f24b5cb6873bf3f9a689f929eb1fbbf0f5
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-08-15 18:31:59 +0000

    Update cmaki.cmake

--- app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoScreen.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoScreen.kt
@@ -44,4 +44,4 @@ fun CoinInfoScreen(
                         )
                         Text(
                             text = if(coin.isActive) "active" else "inactive",
-                            color = if(coin.isActive
+                            color = if(coin.isActive) MaterialTheme.colors.primary else Color.Red,
