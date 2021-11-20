fb71fd83d62e98e1578b846e382216f155cbefa2
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-11-20 13:06:10 +0000

    Port to uic4

--- app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoState.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/CoinInfoState.kt
@@ -2,4 +2,6 @@ package com.example.coindex.presentation.coin_info
 
 import com.example.coindex.domain.model.CoinInfo
 
-data class CoinInfoStat
+data class CoinInfoState (
+    val isLoading: Boolean = false,
+  
