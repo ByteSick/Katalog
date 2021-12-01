60351e1ca3c49ec10e1288241118997e41951550
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-12-01 17:51:57 +0000

    house keeping

--- app/src/main/java/com/example/coindex/presentation/coin_info/components/CoinTag.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/components/CoinTag.kt
@@ -1,4 +1,5 @@
 package com.example.coindex.presentation.coin_info.components
 
 import androidx.compose.foundation.border
-import androidx.compose.founda
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.l
