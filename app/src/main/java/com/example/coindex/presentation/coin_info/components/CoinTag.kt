cce71f8c4f5bc7d12301895db9705be16ff2baf7
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-12-04 06:54:06 +0000

    Updating branch

--- app/src/main/java/com/example/coindex/presentation/coin_info/components/CoinTag.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/components/CoinTag.kt
@@ -2,4 +2,5 @@ package com.example.coindex.presentation.coin_info.components
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.l
+import androidx.compose.foundation.layout.padding
+import androidx.compose
