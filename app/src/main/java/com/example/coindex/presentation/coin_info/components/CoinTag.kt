dd7f6da6386748ef442bba5ff9da1fca9d075a72
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-12-04 09:47:20 +0000

    Documentation updated

--- app/src/main/java/com/example/coindex/presentation/coin_info/components/CoinTag.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/components/CoinTag.kt
@@ -4,4 +4,5 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.M
+import androidx.compose.material.MaterialTheme
+import androidx.c
