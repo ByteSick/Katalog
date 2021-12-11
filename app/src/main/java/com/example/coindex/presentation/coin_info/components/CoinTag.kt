dc03407585542c0321939ec0028c15916d30c4e0
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-12-11 22:35:54 +0000

    logging fixes

--- app/src/main/java/com/example/coindex/presentation/coin_info/components/CoinTag.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/components/CoinTag.kt
@@ -8,4 +8,5 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.
+import androidx.compose.ui.text.style.TextAlign
+import androi
