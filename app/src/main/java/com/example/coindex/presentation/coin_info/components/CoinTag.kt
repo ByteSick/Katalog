55d972a6a962de9d69802db9886251d996fe270f
ByteSick <124615251+ByteSick@users.noreply.github.com>
2021-12-12 08:59:49 +0000

    Update RELEASE.md

--- app/src/main/java/com/example/coindex/presentation/coin_info/components/CoinTag.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/components/CoinTag.kt
@@ -9,4 +9,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androi
+import androidx.compose.ui.unit.dp
+
+@Composable
+fu
