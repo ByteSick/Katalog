7e5e3e55b868d4b665c91c43e305245b398093b9
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-01-01 11:31:54 +0000

    Change license to GPLv3

--- app/src/main/java/com/example/coindex/presentation/coin_info/components/TeamListItem.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/components/TeamListItem.kt
@@ -2,4 +2,4 @@ package com.example.coindex.presentation.coin_info.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-i
+import androidx.compose.foundation.layout.
