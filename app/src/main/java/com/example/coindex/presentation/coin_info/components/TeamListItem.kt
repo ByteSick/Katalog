a2dd824287337dd4367a35ffd8a68dc5db93019d
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-01-01 14:53:21 +0000

    Upgrade yarn.lock

--- app/src/main/java/com/example/coindex/presentation/coin_info/components/TeamListItem.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/components/TeamListItem.kt
@@ -3,4 +3,5 @@ package com.example.coindex.presentation.coin_info.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.heig
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Mat
