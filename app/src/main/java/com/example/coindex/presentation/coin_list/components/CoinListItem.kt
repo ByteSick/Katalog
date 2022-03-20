dcb364e07a86294ff344d0966686302edf18efc8
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-03-20 07:45:12 +0000

    Submodule upload

--- app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
@@ -1,3 +1,4 @@
 package com.example.coindex.presentation.coin_list.components
 
-import androidx.compose.founda
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout
