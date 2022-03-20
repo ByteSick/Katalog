5e128939a9a7487478e907c00429fcc3ff9abf57
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-03-20 22:09:31 +0000

    pattern update

--- app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
@@ -2,4 +2,5 @@ package com.example.coindex.presentation.coin_list.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation
+import androidx.compose.foundation.layout.Row
+import androidx.compose
