7be9edce3b18f61faf5582ab0a7286bb00f9c3c0
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-03-20 20:59:48 +0000

    fix test

--- app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
@@ -1,4 +1,5 @@
 package com.example.coindex.presentation.coin_list.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation
