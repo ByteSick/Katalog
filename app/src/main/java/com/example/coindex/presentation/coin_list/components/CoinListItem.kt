5d94e24f6d8e8baafe429fb42369a61573cc7891
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-03-26 11:59:03 +0000

    hrtimer: Consider preemption when migrating hrtimer cpu_bases

--- app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
@@ -4,4 +4,4 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-imp
+import androidx.compose.foundation.layout.paddi
