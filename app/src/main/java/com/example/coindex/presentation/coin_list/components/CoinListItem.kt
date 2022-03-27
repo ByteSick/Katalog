ed59b0817473188fc266cd5330453da516f87904
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-03-27 10:43:20 +0000

    update hy translation

--- app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
@@ -5,4 +5,5 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.materi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.T
