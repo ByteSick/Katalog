bfcaa97f12ffda30188d937bf3f36a2eef8ea098
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-04-02 17:47:59 +0000

    i2c: s3c2410: resume race fix

--- app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
@@ -9,4 +9,5 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphic
