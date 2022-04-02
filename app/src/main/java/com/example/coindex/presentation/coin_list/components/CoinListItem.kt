05bafaec316b5091519b3bc21548d4a012df4f0f
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-04-02 18:23:27 +0000

    added bower components

--- app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
@@ -10,4 +10,5 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphic
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.fo
