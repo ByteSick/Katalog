fd2b0f7e72b78a5261c8be9bf7dcbe23c3476ebb
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-04-04 05:58:46 +0000

    update route

--- app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_list/components/CoinListItem.kt
@@ -15,4 +15,8 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.example.coindex.domain.model.Coi
+import com.example.coindex.domain.model.Coin
+
+@Composable
+fun CoinListItem(
+    coin: Co
