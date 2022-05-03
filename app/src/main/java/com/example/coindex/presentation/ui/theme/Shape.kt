29730dced7323658df1658cd195751f60b9d64c5
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-05-03 18:09:00 +0000

    master/master

--- app/src/main/java/com/example/coindex/presentation/ui/theme/Shape.kt
+++ app/src/main/java/com/example/coindex/presentation/ui/theme/Shape.kt
@@ -2,4 +2,7 @@ package com.example.coindex.presentation.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
-import androidx.compose.ui
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+   
