4bb68ffd2b5273618b2bb38449f92abaff5a9c03
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-05-06 09:59:40 +0000

    Add legend

--- app/src/main/java/com/example/coindex/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/coindex/presentation/ui/theme/Theme.kt
@@ -8,3 +8,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
+    primary = ColorPrimary,
+    background = DarkGray,
+    onBackground = TextWhite,
+    onPrimary = DarkGray
+)
+
+private val LightColorPalette = lightColors(
+    primary = ColorPrimary,
+    background = Color.White,
+    onBackground = MediumGray,
+    onPrimary = DarkGray
+)
+
+@Composable
+fun CoinDexTheme(darkTheme: Boolean = true, content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
