51e71f73b1e2227d9a184a0bb30cc5fbe6aace06
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-05-11 13:09:54 +0000

    modified layout

--- app/src/main/java/com/example/coindex/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/coindex/presentation/ui/theme/Type.kt
@@ -1,3 +1,4 @@
 package com.example.coindex.presentation.ui.theme
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.Tex
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.
