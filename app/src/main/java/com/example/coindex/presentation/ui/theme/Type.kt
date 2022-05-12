21d04abdf8b1ba29cf04ee12061b22f29c6ee881
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-05-12 07:25:08 +0000

    Updates to the documentation

--- app/src/main/java/com/example/coindex/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/coindex/presentation/ui/theme/Type.kt
@@ -2,4 +2,5 @@ package com.example.coindex.presentation.ui.theme
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.comp
+import androidx.compose.ui.text.font.FontWeight
+import 
