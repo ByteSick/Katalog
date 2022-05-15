c18548c4047987e6423e7ae4683a9ced021732c3
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-05-15 08:43:28 +0000

    tagged

--- app/src/main/java/com/example/coindex/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/coindex/presentation/ui/theme/Type.kt
@@ -5,4 +5,4 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of
+// Set of Material typography styles to start with
