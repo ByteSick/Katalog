d9077071d4a2e8ffdd7f970c68217b3d5931864f
ByteSick <124615251+ByteSick@users.noreply.github.com>
2022-12-15 07:08:28 +0000

    Fix another invocation of git from gitk with an overly long command-line

--- app/src/test/java/com/example/coindex/data/repository/CoinRepositoryImplTest.kt
+++ app/src/test/java/com/example/coindex/data/repository/CoinRepositoryImplTest.kt
@@ -3,4 +3,5 @@ package com.example.coindex.data.repository
 import com.example.coindex.data.remote.CoinApi
 import com.example.coindex.data.remote.dto.CoinDto
 import org.junit.jupiter.api.Test
-import org.junit.j
+import org.junit.jupiter.api.Assertions.*
+import o
